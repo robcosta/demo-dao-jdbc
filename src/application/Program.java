300212d4f192e6f3c592ffe9f820e6e7d50d484d
Roberto Costa <robertocosta66@gmail.com>
2023-10-11 14:06:57 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,10 +28,17 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
-		System.out.println("\n=== Test 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dpt);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n=== Test 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dpt);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		seller.setEmail("martha@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	
 		
 		DB.closeConnection();
