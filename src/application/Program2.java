38dbb0c0f43b23ec71c8af816a0a9035e87c5462
Roberto Costa <robertocosta66@gmail.com>
2023-10-12 08:27:59 -0300

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -26,24 +26,23 @@ public class Program2 {
 		List<Department> list = departmentDao.findAll();
 		list.forEach(System.out::println);
 		
-		System.out.println("\n=== Test 4: seller insert ===");
-		Department newDepartment = new Department(null, "Cleaning");
-		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! New id = " + newDepartment.getId());
-/*		
-		System.out.println("\n=== Test 5: seller update ===");
-		seller = sellerDao.findById(1);
-		seller.setName("Martha Waine");
-		seller.setEmail("martha@gmail.com");
-		sellerDao.update(seller);
+//		System.out.println("\n=== Test 4: department insert ===");
+//		Department newDepartment = new Department(null, "Cleaning");
+//		departmentDao.insert(newDepartment);
+//		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		System.out.println("\n=== Test 5: department update ===");
+		department = departmentDao.findById(8);;
+		department.setName("Plastics");
+		departmentDao.update(department);
 		System.out.println("Update completed");
 		
-		System.out.println("\n=== Test 6: seller delete ===");
+		System.out.println("\n=== Test 6: department delete ===");
 		System.out.print("Enter seller id to be deleted: ");
 		int id = sc.nextInt();
-		sellerDao.deleteById(id);
+		departmentDao.deleteById(id);
 		System.out.println("Delete completed! ");
-		*/
+
 		DB.closeConnection();
 		sc.close();
 		
